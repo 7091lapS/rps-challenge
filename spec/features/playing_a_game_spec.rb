0f14c8a1f3db46c3099be99660ef06633aa7571f
Antonio Voce <antonio.v0c3@gmail.com>
2015-08-10 21:42:59 +0100

    fixes tests to pass CI

--- spec/features/playing_a_game_spec.rb
+++ spec/features/playing_a_game_spec.rb
@@ -5,7 +5,7 @@ feature 'Playing a game' do
     allow_any_instance_of(Computer).to receive(:move).and_return('Paper')
   end
   scenario 'Player can choose an option' do
-    visit '/game?myname=Antonio'
+    visit '/game?playername=Antonio'
     expect(page).to have_content 'Now select an option!'
   end
   scenario 'player can choose to play against cpu' do
@@ -13,29 +13,29 @@ feature 'Playing a game' do
     expect(page).to have_button 'New Game'
   end
   scenario 'player is taken to the game' do
-    visit '/game?myname=Antonio'
+    visit '/game?playername=Antonio'
     expect(page).to have_selector('option')
   end
   scenario 'Player chooses an option' do
-    visit '/game?myname=Antonio'
+    visit '/game?playername=Antonio'
     select 'Rock', from: 'option'
     click_button 'Throw!'
     expect(page).to have_content "You threw Rock!"
   end
   scenario 'Computer throws random option' do
-    visit '/game?myname=Antonio'
+    visit '/game?playername=Antonio'
     select 'Rock', from: 'option'
     click_button 'Throw!'
     expect(page).to have_content "Computer threw Paper!"
   end
   scenario 'Draw' do
-    visit '/game?myname=Antonio'
+    visit '/game?playername=Antonio'
     select 'Paper', from: 'option'
     click_button 'Throw!'
     expect(page).to have_content "Draw!"
   end
   scenario 'A winner is declared' do
-    visit '/game?myname=Antonio'
+    visit '/game?playername=Antonio'
     select 'Rock', from: 'option'
     click_button 'Throw!'
     expect(page).to have_content "Computer wins!"
